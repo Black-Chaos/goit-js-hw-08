c7304e73cea4632fd6e11a2c7e708a53c6f23849
Black Chaos <black.chaos.no1@gmail.com>
2023-07-30 17:29:48 +0300

    feat: add task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
-console.log("SimpleLightbox:", SimpleLightbox)
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const list = document.querySelector('.gallery');
+
+function createElItem(arr) {
+    return arr.map(({ description, original, preview }) => {
+        const item = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = original;
+        item.append(a);
+        const img = document.createElement('img');
+        img.src = preview;
+        img.alt = description;
+        a.append(img);
+        return item
+    });
+}
+list.append(...createElItem(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+    captionType: 'attr',
+  captionsData:	'alt',
+  captionPosition: 'bottom',
+  captionDelay: 300,
+});
